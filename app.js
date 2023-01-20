3840b030a3c9007bf158b08f065a3556b655a0a6
R2H <r2h@AdminisatorsMBP.attlocal.net>
2023-01-20 10:29:07 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//Create a class for a person that has properties for the person's name, age, and address.
+var Person = /** @class */ (function () {
+    function Person(fullName, age, address) {
+        this.fullName = fullName;
+        this.age = age;
+        this.address = address;
+    }
+    //methods
+    //The class should have a method that takes
+    // a string as an argument and returns the person's name and address concatenated together.
+    Person.prototype.getInfo = function () {
+        try {
+            return "".concat(this.fullName, " ").concat(this.address);
+        }
+        catch (error) {
+            return error.message;
+        }
+    };
+    return Person;
+}());
+//creating an object for the car
+var woman = new Person('aida arias', 24, '3807 Willy Ave');
+console.log(woman.getInfo()); //expected output: aida arias 3807 Willy Ave
---
+++ app.ts
@@ -0,0 +1,28 @@
+//Create a class for a person that has properties for the person's name, age, and address.
+class Person{
+    fullName:string;
+    age:number;
+    address:string;
+
+    constructor(fullName:string, age:number,address:string){
+        this.fullName=fullName;
+        this.age= age;
+        this.address=address;
+    }
+
+    //methods
+    //The class should have a method that takes
+    // a string as an argument and returns the person's name and address concatenated together.
+    getInfo(): string{
+        try{
+            return `${this.fullName} ${this.address}`;
+        }catch (error){
+            return error.message;
+        }
+    }
+}
+
+//creating an object for the car
+let woman = new Person('aida arias',24, '3807 Willy Ave')
+
+console.log(woman.getInfo());//expected output: aida arias 3807 Willy Ave
